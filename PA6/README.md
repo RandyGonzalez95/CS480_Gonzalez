ecb450a0aa618a1debba601dfa06a16ff6e5dfa7
Randy <randygonzalez@nevada.unr.edu>
2016-10-07 14:26:44 -0700

    Added PA6 and files

---
+++ PA6/README.md
@@ -0,0 +1,19 @@
+#PA5 Assimp Model Loader
+##Team Members
+###Brett Knadle  
+###Phillip Vong  
+###Randy Gonzalez  
+
+##Compile Instructions
+- To compile the program, make a new directory, name it build
+- cd build
+- Under the build directory type
+- cmake ..
+- Then type
+- make
+
+To run executable, remain under build directory and type  
+./Tutorial ../Model/colorbox.obj
+
+
+##Shader files are under /PA5/shaders directory.
---
+++ PA6/src/object.cpp
@@ -0,0 +1,122 @@
+#include "object.h"
+#include <fstream>
+#include <cstring>
+
+
+Object::Object(char * objFile)
+{
+  // Initialize
+  myScene = NULL;
+
+  // Open File Data
+  if(!Initialize(objFile))
+  {
+    std::cerr<< "OBJ file was not found, ending program";
+    exit(1);
+  }
+
+  SetVertices();
+
+
+  glGenBuffers(1, &VB);
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Geometry.size(), &Geometry[0], GL_STATIC_DRAW);
+
+  glGenBuffers(1, &IB);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+}
+
+Object::~Object()
+{
+  Geometry.clear();
+  Indices.clear();
+}
+
+bool Object::Initialize(char *objFile)
+{
+  // Open File
+  myScene = importer.ReadFile(objFile, aiProcess_Triangulate);
+
+  // check if file exits
+  if(myScene == NULL)
+  {
+    return false;
+  }
+return true;
+}
+
+void Object::SetVertices()
+{
+  // Declare variables
+    // myScene currently holds all .obj data
+    // grab the number of meshes
+  int meshes = myScene->mNumMeshes;// usually just one
+  int faces = 0;
+  int index = 0;
+  Vertex temp;
+  float RGB[3] = {1.0,1.0,1.0};
+
+  // Iterate through mMeshes
+  for(int iMesh = 0; iMesh < meshes; iMesh++) // mesh index iterator
+  {
+
+    // Attempt to extract color
+    // Declare mesh pointer and mtl pointer
+    const aiMesh* model = myScene->mMeshes[iMesh];
+    const aiMaterial *mtl = myScene->mMaterials[model->mMaterialIndex];
+    aiColor4D diffuse; // color values
+
+    // Find number of faces per mesh
+    faces = myScene->mMeshes[iMesh]->mNumFaces;
+    for(int iFaces = 0; iFaces <faces; iFaces++) // face index
+    {
+      // for each face in the mesh
+      for(int i = 0; i<3; i++)
+      {
+          // Grab index info of the faces
+        index = myScene->mMeshes[iMesh]->mFaces[iFaces].mIndices[i];
+        for(int j =0; j<3; j++)  // iterate through each face
+        {
+          // check for materials folder
+          if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_DIFFUSE, &diffuse))
+            {
+              RGB[j] = diffuse[j];
+            }
+          // Index info corresponds to a vertex position
+          temp.position[j] = myScene->mMeshes[iMesh]->mVertices[index][j];
+          temp.color[j] = RGB[j];
+        }
+
+        // Push back Index and Geomtry info
+        Indices.push_back(index);
+        Geometry.push_back(temp);
+      }
+    }
+  }
+
+}
+
+glm::mat4 Object::GetModel()
+{
+
+  //model = glm::rotate(glm::mat4(1.0f), 180.0f, glm::vec3(1.0,0.0,0.0));
+  return model;
+}
+
+void Object::Render()
+{
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,color));
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+
+  glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+}
