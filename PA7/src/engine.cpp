e003d3c5763a053227b25b480570403cf833f1ef
Randy <randygonzalez@nevada.unr.edu>
2016-10-12 10:40:52 -0700

    Added PA7

---
+++ PA7/src/engine.cpp
@@ -0,0 +1,82 @@
+#include "engine.h"
+
+Engine::Engine(string name, int width, int height)
+{
+  m_WINDOW_NAME = name;
+  m_WINDOW_WIDTH = width;
+  m_WINDOW_HEIGHT = height;
+  m_FULLSCREEN = false;
+}
+
+Engine::Engine(string name)
+{
+  m_WINDOW_NAME = name;
+  m_WINDOW_HEIGHT = 0;
+  m_WINDOW_WIDTH = 0;
+  m_FULLSCREEN = true;
+}
+
+Engine::~Engine()
+{
+  delete m_window;
+  delete m_graphics;
+  m_window = NULL;
+  m_graphics = NULL;
+}
+
+bool Engine::Initialize(char * objFile, char* textureFile)
+{
+  // Start a window
+  m_window = new Window();
+  if(!m_window->Initialize(m_WINDOW_NAME, &m_WINDOW_WIDTH, &m_WINDOW_HEIGHT))
+  {
+    printf("The window failed to initialize.\n");
+    return false;
+  }
+
+  // Start the graphics
+  m_graphics = new Graphics();
+  if(!m_graphics->Initialize(m_WINDOW_WIDTH, m_WINDOW_HEIGHT, objFile, textureFile))
+  {
+    printf("The graphics failed to initialize.\n");
+    return false;
+  }
+  // No errors
+  return true;
+}
+
+void Engine::Run()
+{
+  m_running = true;
+
+  while(m_running)
+  {
+    // Check the keyboard input
+    while(SDL_PollEvent(&m_event) != 0)
+    {
+      Keyboard();
+    }
+
+    m_graphics->Render();
+
+    // Swap to the Window
+    m_window->Swap();
+  }
+}
+
+void Engine::Keyboard()
+{
+  if(m_event.type == SDL_QUIT)
+  {
+    m_running = false;
+  }
+  else if (m_event.type == SDL_KEYDOWN)
+  {
+    // handle key down events here
+    if (m_event.key.keysym.sym == SDLK_ESCAPE)
+    {
+      m_running = false;
+    }
+  }
+
+}
---
+++ PA7/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "engine.h"
+
+
+int main(int argc, char **argv)
+{
+  char *objFile;
+  char *textureFile;
+
+  // Check for file input in command line
+  if(argc != 2) // if no more than two arguments
+  {
+    printf("ERROR: Provide necessary Config file: SolarSystem.config\n");
+    return 1;
+  }
+  //else save the obj file
+  objFile = argv[1];
+  textureFile = argv[2];
+
+  // Start an engine and run it then cleanup after
+  Engine *engine = new Engine("Tutorial Window Name", 800, 600);
+  if(!engine->Initialize(objFile, textureFile)) // pass object through
+  {
+    printf("The engine failed to start.\n");
+    delete engine;
+    engine = NULL;
+    return 1;
+  }
+  engine->Run();
+  delete engine;
+  engine = NULL;
+  return 0;
+}
