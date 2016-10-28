44b14a18199082ceb36a06a69f0a90532a17cafe
Randy <randygonzalez@nevada.unr.edu>
2016-10-28 12:39:34 -0700

    Added PA8

---
+++ PA8/include/engine.h
@@ -0,0 +1,33 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <sys/time.h>
+#include <assert.h>
+
+#include "window.h"
+#include "graphics.h"
+
+class Engine
+{
+  public:
+    Engine(string name, int width, int height);
+    Engine(string name);
+    ~Engine();
+    bool Initialize(char *objFile, char* textureFile);
+    void Run();
+    void Keyboard();
+
+  private:
+    // Window related variables
+    Window *m_window;
+    string m_WINDOW_NAME;
+    int m_WINDOW_WIDTH;
+    int m_WINDOW_HEIGHT;
+    bool m_FULLSCREEN;
+    SDL_Event m_event;
+
+    Graphics *m_graphics;
+    bool m_running;
+};
+
+#endif // ENGINE_H
---
+++ PA8/include/object.h
@@ -0,0 +1,35 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+
+class Object
+{
+  public:
+    Object(char *objFile, char *textureFile);
+    ~Object();
+
+    bool Initialize(char *objFile);
+    void SetVertices();
+    void getTextures(char* textureFile);
+    void Render();
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Geometry;
+    std::vector<unsigned int> Indices;
+
+    GLuint Textures;
+    GLuint VB;
+    GLuint IB;
+    // assimp
+    Assimp::Importer importer;
+    const aiScene *myScene;
+};
+
+
+
+#endif /* OBJECT_H */
